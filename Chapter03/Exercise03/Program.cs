64dd1c0c6a7f51b3443e537f986b580a3644911c
Amisha <narvekar.amisha@gmail.com>
2024-07-21 01:29:58 +0530

    day 3

---
+++ Chapter03/Exercise03/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Write("Enter a number: ");
+string inputin = Console.ReadLine();
+if (int.TryParse(inputin, out int number))
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        WriteLine("FizzBuzz");
+    }
+    else if (number % 3 == 0)
+    {
+        WriteLine("Fizz");
+    }
+    else if (number % 5 == 0)
+    {
+        WriteLine("Buzz");
+    }
+}
